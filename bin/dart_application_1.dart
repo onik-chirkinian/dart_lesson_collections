6bc5ba08ed55931133d0e8c29db089ab737b25dc
Onik Chirkinian <onikchirkinian@gmail.com>
2023-02-03 00:46:04 +0300

    swapMatrix

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,4 +1,5 @@
-main() {
+void main(List<String> args) {
+  print("args: $args");
   List<List<int>> matrix = [
     [1, 0, 0],
     [0, 1, 0],
@@ -6,21 +7,40 @@ main() {
     [0, 1, 0],
     [1, 0, 0],
   ];
+
+
+  // final total = countTotalOFMatrix(matrix);
+  // print("total printed of matrix: $total");
   logMatrix(matrix);
   print("swaping");
-  for (final row in matrix) {
+  // for (final row in matrix) {
     // print("row: $row");
-    final temp = row[0];
-    row[0] = row[2];
-    row[2] = temp;
+    // final temp = row[0];
+    // row[0] = row[2];
+    // row[2] = temp;
     // print("new row: $row");
   }
+
   logMatrix(matrix);
   print("");
 }
 
-void logMatrix(List<List<int>> matrix) {
+
+
+
+/// calculates total sum of all elements in the matrix
+int countTotalOFMatrix(List<List<int>> matrix) {
+  int total = 0;
   for (final row in matrix) {
+    for (final rowItem in row) {
+      total = total + rowItem;
+    }
+  }
+  return total;
+}
+
+void logMatrix(List<List<int>> m) {
+  for (final row in m) {
     print("row: $row");
   }
 }
@@ -34,21 +54,13 @@ void logMatrixRow(List<List<int>> matrix, int rowIndex) {
     print("index: $index more then matrix max index (${matrix.length - 1})");
   }
 }
- 
 
-
-
-
-
-
-
-
-
-//main() {
-//  List<List<int>> matrix = [
-  //  [1, 0, 0],
-   // [0, 1, 0],
-   // [0, 0, 1],
-   // [0, 1, 0],
-   // [1, 0, 0],
-//for (final tmp = Item(0);;);];
+void swapMatrix(List<List<int>> matrix) {
+  for (final row in matrix) {
+    // print("row: $row");
+    final temp = row[0];
+    row[0] = row[2];
+    row[2] = temp;
+    // print("new row: $row");
+  }
+}
