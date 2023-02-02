92245fc312e10729626315c0e2669acd3cd3f577
Onik Chirkinian <onikchirkinian@gmail.com>
2023-02-02 23:35:18 +0300

    print row

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -7,8 +7,13 @@ main() {
     [1, 0, 0],
   ];
 
-  final row = matrix[0];
-  print("row1: $row");
+  final index = 5;
+  if (index < matrix.length) {
+    final row = matrix[index];
+    print("row1: $row");
+  } else {
+    print("index: $index more then matrix max index (${matrix.length - 1})");
+  }
 
   for (final row in matrix) {
     // print("row: $row");
