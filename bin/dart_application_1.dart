308eaef03815bd3542f607936615f95e3b6e42b1
Onik Chirkinian <onikchirkinian@gmail.com>
2023-02-02 23:54:07 +0300

    add function logMatrix

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -6,7 +6,26 @@ main() {
     [0, 1, 0],
     [1, 0, 0],
   ];
+  logMatrix(matrix);
+  print("swaping");
+  for (final row in matrix) {
+    // print("row: $row");
+    final temp = row[0];
+    row[0] = row[2];
+    row[2] = temp;
+    // print("new row: $row");
+  }
+  logMatrix(matrix);
+  print("");
+}
 
+void logMatrix(List<List<int>> matrix) {
+  for (final row in matrix) {
+    print("row: $row");
+  }
+}
+
+void logMatrixRow(List<List<int>> matrix, int rowIndex) {
   final index = 5;
   if (index < matrix.length) {
     final row = matrix[index];
@@ -14,21 +33,7 @@ main() {
   } else {
     print("index: $index more then matrix max index (${matrix.length - 1})");
   }
-
-  for (final row in matrix) {
-    print("row: $row");
-    final temp = row[0];
-    row[0] = row[2];
-    row[2] = temp;
-    print("new row: $row");
-  }
-
-  print(matrix);
-  print("");
 }
-
-
-
  
 
 
