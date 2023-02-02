64923dd081863edd19c306503310471b3e9e9ded
Onik Chirkinian <onikchirkinian@gmail.com>
2023-02-03 01:31:28 +0300

    firststep

--- bin/magic_game.dart
+++ bin/magic_game.dart
@@ -1,3 +1,8 @@
-void main(List<String> args) {
-  
+import 'dart:math';
+
+main(List<String> args) {
+  const maxMagic = 100;
+  final random = Random(DateTime.now().microsecondsSinceEpoch);
+  final magicNumber = random.nextInt(maxMagic);
+  print('magicNumber: $magicNumber');
 }
