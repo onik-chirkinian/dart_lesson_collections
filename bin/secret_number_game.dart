42edd6af65ce81334187d09cdef79240524f347b
Onik Chirkinian <onikchirkinian@gmail.com>
2023-02-09 00:24:50 +0300

    secret number  game | first version

---
+++ bin/secret_number_game.dart
@@ -0,0 +1,39 @@
+import 'dart:math';
+import 'dart:convert';
+import 'dart:io';
+
+main() {
+  const maxMagic = 100;
+  final random = Random(DateTime.now().microsecondsSinceEpoch);
+  final secretNumber = random.nextInt(maxMagic);
+  print('write number from 1 to 100. If ur number matches, then you will win. You have three attempts');
+
+  int attempts = 0;
+  int maxAttempts = 3;
+  bool next = true;
+  for (int index = 0; next; index++) {
+    final inputValue = stdin.readLineSync(encoding: utf8);
+    print('your number: "$inputValue"');
+    if (inputValue != null && inputValue != '') {
+      final inputNum = int.tryParse(inputValue);
+      if (inputNum != null) {
+        if (inputNum == secretNumber) {
+          print('you are win!');
+          next = false;
+        } else {
+          print('wrong, try again');
+          if (attempts < (maxAttempts - 1)) {
+            attempts++;
+          } else {
+            next = false;
+            print('you loose, secret number is "$secretNumber"');
+          }
+        }
+      } else {
+        print('wrong input, try another number');
+      }
+    } else {
+      print('wrong input, try another number');
+    }
+  }
+}
