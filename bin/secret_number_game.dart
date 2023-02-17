166f7b0aca607f02a3a6102ac242592cc4b18058
Onik Chirkinian <onikchirkinian@gmail.com>
2023-02-17 22:38:47 +0300

    soloupdate

--- bin/secret_number_game.dart
+++ bin/secret_number_game.dart
@@ -1,30 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'get_input_number.dart';
+
 import 'generate_secret_number.dart';
+import 'winloose.dart';
 
 main() {
   final secretNumber = generateSecretNumber(100);
   print('write number from 1 to 100. If ur number matches, then you will win. You have three attempts');
-  int attempts = 0;
-  int maxAttempts = 3;
-  bool next = true;
-  for (int index = 0; next; index++) {
-    int inputNum = getInputNumber();
-    if (inputNum == secretNumber) {
-      print('you are win!');
-      next = false;
-    } else {
-      print('wrong, try again');
-      if (attempts < (maxAttempts - 1)) {
-        attempts++;
-      } else {
-        next = false;
-        print('you loose, secret number is "$secretNumber"');
-      }
-    }
-  }
+  winloose();
   print('press enter to exit');
   stdin.readLineSync(encoding: utf8);
 }
